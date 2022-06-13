054d6e9b30f023690e1b94b65e1996048758782a
Appuchia <46938448+appuchias@users.noreply.github.com>
2022-06-13 16:24:51 +0200

    Update files

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -1,29 +1,3 @@
-#!/usr/bin/fish
-
-starship init fish | source
-
-# Custom greeting
-function fish_greeting
-    colorscript random
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 end
-
-# Key bindings
-bind \e\[3\;5~ kill-word
-bind \e\[3\;3~ kill-word
-bind \cH backward-kill-word
-
-# Custom path folders
-if [ -f ~/.shell_env ];
-    source ~/.shell_env
-end
-
-# Add aliases
-if [ -f ~/.shell_aliases ];
-    source ~/.shell_aliases
-end
-
-# Add scripts
-if [ -f ~/.shellrc ];
-    source ~/.shellrc
-end
-
