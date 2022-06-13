47466e581c771cbdebb1d3868ea31d54803836b5
Appuchia <46938448+appuchias@users.noreply.github.com>
2022-06-13 16:29:44 +0200

    Update files

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -1,34 +1,3 @@
-#!/usr/bin/fish
-
-starship init fish | source
-
-# Custom path folders
-fish_add_path ~/custompath/
-fish_add_path ~/flutter/bin
-fish_add_path ~/.dotfiles/
-fish_add_path ~/.local/bin/
-
-# Key bindings
-# bind \e\[3\;5~ kill-word
-# bind \cH backward-kill-word
-
-# Add aliases
-if [ -f ~/.shell_aliases ];
-    source ~/.shell_aliases
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 end
-
-# Add scripts
-if [ -f ~/.shellrc ];
-    source ~/.shellrc
-end
-
-# Custom greeting
-function fish_greeting
-    #colorscript -e colorview
-    colorscript random
-    #echo ""
-end
-
-# ENV
-export SUDO_PROMPT='[sudo] %p : '
-# export SUDO_PROMPT='sudo passwd: '
